8848b9c6891c41f5898de85ac5d5f61c60e131a1
Stanislaw Krowicki <krowicki00@gmail.com>
2022-08-14 12:10:17 +0200

    Remove TODO for suspicious events

--- src/resources/rpg/shared/events/ClientEvent.ts
+++ src/resources/rpg/shared/events/ClientEvent.ts
@@ -11,7 +11,6 @@ import {Event, EventType} from "./Events"
 export abstract class ClientEvent extends Event {
     protected static eventType = EventType.CLIENT
 
-    // TODO: Every event should be checked, suspicious ones logged to Mongo
     /// #if SERVER
     static onHandle(client: altServer.Player, object: ClientEvent): void {}
     /// #endif
