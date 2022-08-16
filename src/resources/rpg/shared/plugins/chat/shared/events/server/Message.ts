18194f142df21afcdb669eeda93e406b867df07b
dziemdobry <104797750+dziemdobry@users.noreply.github.com>
2022-08-16 07:48:54 +0000

    Deprecated plugin support

--- src/resources/rpg/shared/plugins/chat/shared/events/server/Message.ts
+++
@@ -1,9 +0,0 @@
-// import { ServerEvent } from '../../../../../events/Events'
-//
-// export default class Message extends ServerEvent {
-//     message: string
-//
-//     /// #if CLIENT
-//     onHandle(): void {}
-//     /// #endif
-// }
