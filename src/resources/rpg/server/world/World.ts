b8aea2b53c66f58f7089959e0273cde89937a7e7
dziemdobry <104797750+dziemdobry@users.noreply.github.com>
2022-08-21 11:04:55 +0000

    Fixed uninitialized variable definition

--- src/resources/rpg/server/world/World.ts
+++ src/resources/rpg/server/world/World.ts
@@ -2,6 +2,6 @@ import type Vehicle from './Vehicle'
 import type { Client } from '../index'
 
 export default class World {
-    static vehicles: Vehicle[]
-    static players: Client[]
+    static vehicles: Vehicle[] = []
+    static players: Client[] = []
 }
