80cb083077502c2b0fbf60ca810245064ae38f97
Stanislaw Krowicki <krowicki00@gmail.com>
2022-08-29 19:14:05 +0200

    Fix edge case calculating yaw when diff y is equal to 0

--- src/resources/rpg/shared/utils/Utils.ts
+++ src/resources/rpg/shared/utils/Utils.ts
@@ -17,12 +17,18 @@ export default class {
         const dz = src.z - target.z
 
         const pitch = -Math.atan2(dz, Math.sqrt(dx * dx + dy * dy))
-        let yaw = Math.atan2(dy, dx)
+        let yaw
 
-        if (dy > 0) {
+        if (dy === 0) {
+            if (dx > 0) yaw = Math.PI/2
+            if (dx < 0) yaw = -Math.PI/2
+        }
+        else if (dy >= 0) {
+            yaw = Math.atan2(dy, dx)
             if (yaw > 0) yaw += Math.PI/2
             else if (yaw < 0) yaw -= Math.PI/2
         } else if (dy < 0) {
+            yaw = Math.atan2(dy, dx)
             if (yaw > 0) yaw -= Math.PI/2
             else if (yaw < 0) yaw += Math.PI/2
         }
