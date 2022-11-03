b9204e7add9b16ffa1c0f43764729f0031375bb2
Stanislaw Krowicki <krowicki00@gmail.com>
2022-11-03 18:19:05 +0100

    Bind change prompt

--- src/resources/rpg/client/webviews/settings/BindsSettings.svelte
+++ src/resources/rpg/client/webviews/settings/BindsSettings.svelte
@@ -1,8 +1,61 @@
 <script lang="ts">
+    import { createEventDispatcher } from 'svelte'
+
     import KeyboardUtils from 'rpg/shared/utils/KeyboardUtils'
     import type { IBindDefinition } from 'rpg/client/settings/ClientSettings'
 
+    import { settingsPanel as trans } from 'lang'
+
+    interface IBindChange {
+        key: string
+        modifier?: 'Control' | 'Alt' | 'Shift'
+        bindName: string
+    }
+
     export let availableBinds: IBindDefinition[] = []
+
+    const dispatch = createEventDispatcher()
+
+    let showBindPrompt = false
+    let currentlyBinding: string = undefined
+
+    const bindChangePrompt = (bindName: string) => {
+        if (showBindPrompt) return
+
+        showBindPrompt = true
+        currentlyBinding = bindName
+        document.addEventListener('keydown', onChangePromptKeypress)
+    }
+
+    const onChangePromptKeypress = (event: KeyboardEvent) => {
+        if (['Alt', 'Shift', 'Control'].includes(event.key)) {
+            return
+        }
+
+        let sanitizedKey = event.key
+
+        // make caps lock not matter as it does not in altV
+        if (event.key.length === 1) sanitizedKey = event.key.toUpperCase()
+
+        if (typeof KeyboardUtils.getKeyCodeFromChar(sanitizedKey) === 'undefined') {
+            return
+        }
+
+        let modifier: 'Control' | 'Alt' | 'Shift' = undefined
+
+        if (event.ctrlKey) modifier = 'Control'
+        else if (event.shiftKey) modifier = 'Shift'
+        else if (event.altKey) modifier = 'Alt'
+
+        onChangeFinish({ key: sanitizedKey, modifier: modifier, bindName: currentlyBinding })
+    }
+
+    const onChangeFinish = (changedBind: IBindChange) => {
+        dispatch('bindChange', changedBind)
+        document.removeEventListener('keydown', onChangePromptKeypress)
+        showBindPrompt = false
+        currentlyBinding = undefined
+    }
 </script>
 
 <div class="container">
@@ -14,9 +67,16 @@
                 name={bind.name}
                 value={KeyboardUtils.getTranslatedCharFromKeyCode(bind.keyCode)}
                 readonly
+                on:click={() => bindChangePrompt(bind.name)}
             />
         </div>
     {/each}
+
+    {#if showBindPrompt}
+        <div class="prompt">
+            <span>{trans.setBind}</span>
+        </div>
+    {/if}
 </div>
 
 <style lang="scss">
@@ -48,6 +108,28 @@
             text-align: center;
             font-family: Roboto, Arial, Helvetica, sans-serif;
             font-weight: 600;
+        }
+    }
+
+    .prompt {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 50em;
+        height: 30em;
+        border: solid 5px $THEME-COLOR-A;
+        font-family: Roboto, Arial, Helvetica, sans-serif;
+        font-weight: 600;
+        background-color: #444;
+
+        span {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: white;
+            font-size: 3em;
         }
     }
 </style>
