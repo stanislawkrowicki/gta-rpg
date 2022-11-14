1f8d0c8b334f21377b32b53f70c7115e0202e712
Stanislaw Krowicki <krowicki00@gmail.com>
2022-11-14 18:14:55 +0100

    Add pre reload actions to HotReload

--- src/resources/rpg/server/HotReload.ts
+++ src/resources/rpg/server/HotReload.ts
@@ -1,6 +1,8 @@
 import watch from 'node-watch'
 import path from 'path'
 import alt from 'alt-server'
+import Clients from './core/client/Clients'
+import Sessions from './core/sessions/Sessions'
 
 export default class HotReload {
     static timeout: any
@@ -11,19 +13,26 @@ export default class HotReload {
             { recursive: true, delay: 100 },
             function (event, strPath) {
                 if (!this.timeout) {
-                    this.timeout = setTimeout(() => {
-                        HotReload.reload(strPath)
+                    this.timeout = setTimeout(async () => {
+                        await HotReload.reload(strPath)
                     }, 500)
                 }
             }
         )
     }
 
-    static reload(strPath: string) {
-        alt.log('~c~' + `Detected changes in resource, reloading...`)
+    static async reload(strPath: string) {
+        alt.log('~c~' + `Detected changes in resource, running pre reload actions...`)
+        HotReload.runPreReloadActions()
 
         // force altv-esbuild to reload the resource
         // @ts-ignore
         alt.emit('consoleCommand', 'res')
+    }
+
+    static async runPreReloadActions() {
+        for (let i = 0; i < Clients.length; i++) {
+            Sessions.updateClientPlayedTimeTotal(Clients[i])
+        }
     }
 }
