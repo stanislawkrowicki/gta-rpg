91673b686c29386b505030d33b773ca30ed84bba
dziemdobry <104797750+dziemdobry@users.noreply.github.com>
2022-12-26 12:51:31 +0000

    Added new methods to Matrix4 and Vector3

--- src/resources/rpg/shared/utils/Matrix4.ts
+++ src/resources/rpg/shared/utils/Matrix4.ts
@@ -36,22 +36,10 @@ export default class Matrix4 {
         c4 = 0,
         d4 = 0
     ) {
-        this.a1 = a1
-        this.b1 = b1
-        this.c1 = c1
-        this.d1 = d1
-        this.a2 = a2
-        this.b2 = b2
-        this.c2 = c2
-        this.d2 = d2
-        this.a3 = a3
-        this.b3 = b3
-        this.c3 = c3
-        this.d3 = d3
-        this.a4 = a4
-        this.b4 = b4
-        this.c4 = c4
-        this.d4 = d4
+        ;(this.a1 = a1), (this.b1 = b1), (this.c1 = c1), (this.d1 = d1)
+        ;(this.a2 = a2), (this.b2 = b2), (this.c2 = c2), (this.d2 = d2)
+        ;(this.a3 = a3), (this.b3 = b3), (this.c3 = c3), (this.d3 = d3)
+        ;(this.a4 = a4), (this.b4 = b4), (this.c4 = c4), (this.d4 = d4)
     }
 
     getX() {
@@ -110,9 +98,170 @@ export default class Matrix4 {
             this.a4 * matrix.a1 + this.b4 * matrix.a2 + this.c4 * matrix.a3 + this.d4 * matrix.a4,
             this.a4 * matrix.b1 + this.b4 * matrix.b2 + this.c4 * matrix.b3 + this.d4 * matrix.b4,
             this.a4 * matrix.c1 + this.b4 * matrix.c2 + this.c4 * matrix.c3 + this.d4 * matrix.c4,
-            this.a4 * matrix.d1 + this.b4 * matrix.d2 + this.c4 * matrix.d3 + this.d4 * matrix.d4,
+            this.a4 * matrix.d1 + this.b4 * matrix.d2 + this.c4 * matrix.d3 + this.d4 * matrix.d4
         )
 
         return product
+    }
+
+    getInverse() {
+        const inverse = new Matrix4()
+
+        const n11 = this.a1,
+            n21 = this.b1,
+            n31 = this.c1,
+            n41 = this.d1
+        const n12 = this.a2,
+            n22 = this.b2,
+            n32 = this.c2,
+            n42 = this.d2
+        const n13 = this.a3,
+            n23 = this.b3,
+            n33 = this.c3,
+            n43 = this.d3
+        const n14 = this.a4,
+            n24 = this.b4,
+            n34 = this.c4,
+            n44 = this.d4
+
+        const t11 =
+            n23 * n34 * n42 -
+            n24 * n33 * n42 +
+            n24 * n32 * n43 -
+            n22 * n34 * n43 -
+            n23 * n32 * n44 +
+            n22 * n33 * n44
+        const t12 =
+            n14 * n33 * n42 -
+            n13 * n34 * n42 -
+            n14 * n32 * n43 +
+            n12 * n34 * n43 +
+            n13 * n32 * n44 -
+            n12 * n33 * n44
+        const t13 =
+            n13 * n24 * n42 -
+            n14 * n23 * n42 +
+            n14 * n22 * n43 -
+            n12 * n24 * n43 -
+            n13 * n22 * n44 +
+            n12 * n23 * n44
+        const t14 =
+            n14 * n23 * n32 -
+            n13 * n24 * n32 -
+            n14 * n22 * n33 +
+            n12 * n24 * n33 +
+            n13 * n22 * n34 -
+            n12 * n23 * n34
+
+        const d = n11 * t11 + n21 * t12 + n31 * t13 + n41 * t14
+
+        if (d === 0) {
+            throw 'Matrix is not invertible'
+        }
+
+        const detInv = 1 / d
+
+        inverse.a1 = t11 * detInv
+        inverse.b1 =
+            (n24 * n33 * n41 -
+                n23 * n34 * n41 -
+                n24 * n31 * n43 +
+                n21 * n34 * n43 +
+                n23 * n31 * n44 -
+                n21 * n33 * n44) *
+            detInv
+        inverse.c1 =
+            (n22 * n34 * n41 -
+                n24 * n32 * n41 +
+                n24 * n31 * n42 -
+                n21 * n34 * n42 -
+                n22 * n31 * n44 +
+                n21 * n32 * n44) *
+            detInv
+        inverse.d1 =
+            (n23 * n32 * n41 -
+                n22 * n33 * n41 -
+                n23 * n31 * n42 +
+                n21 * n33 * n42 +
+                n22 * n31 * n43 -
+                n21 * n32 * n43) *
+            detInv
+        inverse.a2 = t12 * detInv
+        inverse.b2 =
+            (n13 * n34 * n41 -
+                n14 * n33 * n41 +
+                n14 * n31 * n43 -
+                n11 * n34 * n43 -
+                n13 * n31 * n44 +
+                n11 * n33 * n44) *
+            detInv
+        inverse.c2 =
+            (n14 * n32 * n41 -
+                n12 * n34 * n41 -
+                n14 * n31 * n42 +
+                n11 * n34 * n42 +
+                n12 * n31 * n44 -
+                n11 * n32 * n44) *
+            detInv
+        inverse.d2 =
+            (n12 * n33 * n41 -
+                n13 * n32 * n41 +
+                n13 * n31 * n42 -
+                n11 * n33 * n42 -
+                n12 * n31 * n43 +
+                n11 * n32 * n43) *
+            detInv
+        inverse.a3 = t13 * detInv
+        inverse.b3 =
+            (n14 * n23 * n41 -
+                n13 * n24 * n41 -
+                n14 * n21 * n43 +
+                n11 * n24 * n43 +
+                n13 * n21 * n44 -
+                n11 * n23 * n44) *
+            detInv
+        inverse.c3 =
+            (n12 * n24 * n41 -
+                n14 * n22 * n41 +
+                n14 * n21 * n42 -
+                n11 * n24 * n42 -
+                n12 * n21 * n44 +
+                n11 * n22 * n44) *
+            detInv
+        inverse.d3 =
+            (n13 * n22 * n41 -
+                n12 * n23 * n41 -
+                n13 * n21 * n42 +
+                n11 * n23 * n42 +
+                n12 * n21 * n43 -
+                n11 * n22 * n43) *
+            detInv
+        inverse.a4 = t14 * detInv
+        inverse.b4 =
+            (n13 * n24 * n31 -
+                n14 * n23 * n31 +
+                n14 * n21 * n33 -
+                n11 * n24 * n33 -
+                n13 * n21 * n34 +
+                n11 * n23 * n34) *
+            detInv
+        inverse.c4 =
+            (n14 * n22 * n31 -
+                n12 * n24 * n31 -
+                n14 * n21 * n32 +
+                n11 * n24 * n32 +
+                n12 * n21 * n34 -
+                n11 * n22 * n34) *
+            detInv
+        inverse.d4 =
+            (n12 * n23 * n31 -
+                n13 * n22 * n31 +
+                n13 * n21 * n32 -
+                n11 * n23 * n32 -
+                n12 * n21 * n33 +
+                n11 * n22 * n33) *
+            detInv
+
+        return inverse
     }
 }
--- src/resources/rpg/shared/utils/Vector3.ts
+++ src/resources/rpg/shared/utils/Vector3.ts
@@ -1,5 +1,6 @@
 import type altShared from 'alt-shared'
 import type Matrix3 from './Matrix3'
+import type Matrix4 from './Matrix4'
 
 export default class Vector3 {
     x: number
@@ -69,69 +70,172 @@ export default class Vector3 {
         return product
     }
 
-    set(vec: Vector3) {
-        this.x = vec.x
-        this.y = vec.y
-        this.z = vec.z
+    set(vector: Vector3 | altShared.Vector3) {
+        this.x = vector.x
+        this.y = vector.y
+        this.z = vector.z
     }
 
-    add(vec: Vector3) {
-        this.x += vec.x
-        this.y += vec.y
-        this.z += vec.z
-
-        return this
-    }
-    sub(vec: Vector3) {
-        this.x -= vec.x
-        this.y -= vec.y
-        this.z -= vec.z
-
-        return this
-    }
-    div(vec: Vector3) {
-        this.x /= vec.x
-        this.y /= vec.y
-        this.z /= vec.z
-
-        return this
-    }
-    mul(vec: Vector3) {
-        this.x *= vec.x
-        this.y *= vec.y
-        this.z *= vec.z
+    add(vector: Vector3 | altShared.Vector3) {
+        this.x += vector.x
+        this.y += vector.y
+        this.z += vector.z
 
         return this
     }
 
-    dot(vec: Vector3) {
+    addTo(vector: Vector3 | altShared.Vector3, output: Vector3) {
+        Vector3.addTo(this, vector, output)
+    }
+
+    static addTo(a: Vector3 | altShared.Vector3, b: Vector3 | altShared.Vector3, output: Vector3) {
+        output.x = a.x + b.x
+        output.y = a.y + b.y
+        output.z = a.z + b.z
+
+        return output
+    }
+
+    sub(vector: Vector3) {
+        this.x -= vector.x
+        this.y -= vector.y
+        this.z -= vector.z
+
+        return this
+    }
+
+    subTo(vector: Vector3 | altShared.Vector3, output: Vector3) {
+        Vector3.subTo(this, vector, output)
+    }
+
+    static subTo(a: Vector3 | altShared.Vector3, b: Vector3 | altShared.Vector3, output: Vector3) {
+        output.x = a.x - b.x
+        output.y = a.y - b.y
+        output.z = a.z - b.z
+
+        return output
+    }
+
+    div(vector: Vector3) {
+        this.x /= vector.x
+        this.y /= vector.y
+        this.z /= vector.z
+
+        return this
+    }
+
+    divTo(vector: Vector3 | altShared.Vector3, output: Vector3) {
+        Vector3.divTo(this, vector, output)
+    }
+
+    static divTo(a: Vector3 | altShared.Vector3, b: Vector3 | altShared.Vector3, output: Vector3) {
+        output.x = a.x / b.x
+        output.y = a.y / b.y
+        output.z = a.z / b.z
+
+        return output
+    }
+
+    mul(vector: Vector3) {
+        this.x *= vector.x
+        this.y *= vector.y
+        this.z *= vector.z
+
+        return this
+    }
+
+    mulTo(vector: Vector3 | altShared.Vector3, output: Vector3) {
+        Vector3.mulTo(this, vector, output)
+    }
+
+    static mulTo(a: Vector3 | altShared.Vector3, b: Vector3 | altShared.Vector3, output: Vector3) {
+        output.x = a.x * b.x
+        output.y = a.y * b.y
+        output.z = a.z * b.z
+
+        return output
+    }
+
+    dot(vector: Vector3 | altShared.Vector3) {
+        return Vector3.dot(this, vector)
+    }
+
+    static dot(a: Vector3 | altShared.Vector3, b: Vector3 | altShared.Vector3) {
         let product = 0
 
-        product += this.x * vec.x
-        product += this.y * vec.y
-        product += this.z * vec.z
+        product += a.x * b.x
+        product += a.y * b.y
+        product += a.z * b.z
 
         return product
     }
 
     dotFromMatrix3(matrix: Matrix3) {
-        const product = new Vector3(
-            matrix.a1 * this.x + matrix.a2 * this.y + matrix.a3 * this.z,
-            matrix.b1 * this.x + matrix.b2 * this.y + matrix.b3 * this.z,
-            matrix.c1 * this.x + matrix.c2 * this.y + matrix.c3 * this.z
-        )
+        const product = new Vector3()
+
+        this.dotFromMatrix3To(matrix, product)
 
         return product
     }
 
-    cross(vec: Vector3) {
+    dotFromMatrix3To(matrix: Matrix3, productOutput: Vector3) {
+        Vector3.dotFromMatrix3To(this, matrix, productOutput)
+    }
+
+    static dotFromMatrix3To(
+        vector: Vector3 | altShared.Vector3,
+        matrix: Matrix3,
+        productOutput: Vector3
+    ) {
+        productOutput.x = matrix.a1 * vector.x + matrix.a2 * vector.y + matrix.a3 * vector.z
+        productOutput.y = matrix.b1 * vector.x + matrix.b2 * vector.y + matrix.b3 * vector.z
+        productOutput.z = matrix.c1 * vector.x + matrix.c2 * vector.y + matrix.c3 * vector.z
+    }
+
+    cross(vector: Vector3 | altShared.Vector3) {
         const product = new Vector3()
 
-        product.x = this.y * vec.z - this.z * vec.y
-        product.y = this.z * vec.x - this.x * vec.z
-        product.z = this.x * vec.y - this.y * vec.x
+        this.crossTo(vector, product)
 
         return product
+    }
+
+    crossTo(vector: Vector3 | altShared.Vector3, productOutput: Vector3) {
+        Vector3.crossTo(this, vector, productOutput)
+    }
+
+    static crossTo(
+        a: Vector3 | altShared.Vector3,
+        b: Vector3 | altShared.Vector3,
+        productOutput: Vector3
+    ) {
+        productOutput.x = a.y * b.z - a.z * b.y
+        productOutput.y = a.z * b.x - a.x * b.z
+        productOutput.z = a.x * b.y - a.y * b.x
+    }
+
+    unprojectTo(matrix: Matrix4, outputVector: Vector3) {
+        Vector3.unprojectTo(this, matrix, outputVector)
+    }
+
+    static unprojectTo(
+        vector: Vector3 | altShared.Vector3,
+        matrix: Matrix4,
+        outputVector: Vector3
+    ) {
+        const invertedProjectionMatrix = matrix.getInverse()
+
+        outputVector.applyMatrix4(invertedProjectionMatrix)
+    }
+
+    applyMatrix4(matrix: Matrix4) {
+        this.applyMatrix4To(matrix, this)
+    }
+
+    applyMatrix4To(matrix: Matrix4, outputVector: Vector3) {
+        outputVector.x = matrix.a1 * this.x + matrix.b1 * this.y + matrix.c1 * this.z + matrix.d1
+        outputVector.y = matrix.a2 * this.x + matrix.b2 * this.y + matrix.c2 * this.z + matrix.d2
+        outputVector.z = matrix.a3 * this.x + matrix.b3 * this.y + matrix.c3 * this.z + matrix.d3
     }
 
     static getDistanceBetweenTwoXYZPoints(
@@ -151,13 +255,9 @@ export default class Vector3 {
     }
 
     static getDistanceBetweenTwoVectors(
-        a: altShared.Vector3 | Vector3,
-        b: altShared.Vector3 | Vector3
+        a: Vector3 | altShared.Vector3,
+        b: Vector3 | altShared.Vector3
     ): number {
-        const dx = b.x - a.x
-        const dy = b.y - a.y
-        const dz = b.z - a.z
-
-        return Math.sqrt(dx * dx + dy * dy + dz * dz)
+        return Vector3.getDistanceBetweenTwoXYZPoints(a.x, a.y, a.z, b.x, b.y, b.z)
     }
 }
